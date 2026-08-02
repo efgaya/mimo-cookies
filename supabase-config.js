9acaf3bfb4791aa8de93f3da1fb844d401ec24c4
efgaya <efgaya@gmail.com>
2026-08-01 21:56:08 -0300

    Create supabase-config.js
    Adiciona configuração do Supabase

---
+++ supabase-config.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://gixpumwhnvsetohjsmoj.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_YCqPpZ59l-p3Q3PG8ROwYA__daLt-SI";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
